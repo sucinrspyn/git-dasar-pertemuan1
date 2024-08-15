0dd65bfaac92179934b04217b806989cc5dbf708
suci <sopyansuci042@gmail.com>
2024-08-15 12:10:22 +0700

    menambah file index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+$nama = "Suci";
+$umur = "17";
+$ttl = "Sumedang,2 November 200*";
+$askot = "Sumedang";
+$asalsekolah = "Smk negeri 2 sumedang";
+
+echo " ";
+echo "Name : $nama" . "<br>";
+echo "Umur : $umur" . "<br>";
+echo "Ttl : $ttl" . "<br>";
+echo "Askot : $askot" . "<br>";
+echo "Asalsekolah : $asalsekolah" . "<br>";
+
+
+
+// *(tipe data boolean)
+// echo "Benar";
+// echo true;
+
+// echo "False";
+// echo false;
+
+
+
+// *(tipe data Double quete)
+//$nama = " Suci";
+//echo 'nama : ';
+//echo 'Suci $nama ucii';
+//echo "\n";
